1469d0a26580dfa26d517b32b80f8740133b4e98
KunalB96 <kunalbhavsar96@gmail.com>
2022-03-30 13:21:13 +0530

    Done

--- Demo/src/test/Test3.java
+++ Demo/src/test/Test3.java
@@ -1,9 +1,9 @@
 package test;
 
-public class Test {
+public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("Test file modified");
+		System.out.println("New file is added");
 
 	}
 
